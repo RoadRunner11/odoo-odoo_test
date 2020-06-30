b56a3e52a786be718c7ed3187f2510bc0a458417
Akorede Fodilu Olawale <afordeal88@gmail.com>
2020-06-30 20:56:57 +0000

    correct compute

--- odoo_test/models/odoo_test.py
+++ odoo_test/models/odoo_test.py
@@ -7,6 +7,6 @@ class OdooTest(models.Model):
 	full_name = fields.Char(compute='_compute_name', store= True)
 
 	@api.depends('first_name', 'last_name')	
-	def _comput_name(self):
+	def _compute_name(self):
 		for record in self:
 			record.full_name = str(record.first_name) + " " + str(record.last_name)
